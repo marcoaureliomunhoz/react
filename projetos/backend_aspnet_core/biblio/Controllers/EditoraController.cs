2a6eb73e304cff211b4a026cd62deff27b4df675
marcoaureliomunhoz <marcoaureliomunhoz@gmail.com>
2018-08-04 15:40:47 -0300

    .

--- projetos/backend_aspnet_core/biblio/Controllers/EditoraController.cs
+++ projetos/backend_aspnet_core/biblio/Controllers/EditoraController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using biblio.DB;
+using biblio.DTOs;
 using biblio.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
@@ -24,31 +25,59 @@ namespace biblio.Controllers
         }
 
         [HttpGet("editoras")]
-        public IEnumerable<Editora> Get()
+        public IEnumerable<EditoraDTO> Get()
         {
-            return _db.Editoras.Find(_ => true).ToList();
+            var editoras = _db.Editoras.Find(_ => true).ToList();
+            var dtos = new List<EditoraDTO>();
+            editoras.ForEach(x => dtos.Add(new EditoraDTO
+            {
+                Id = x.EditoraId,
+                Nome = x.Nome,
+                Email = x.Email
+            }));
+            return dtos;
         }
 
         [HttpGet("editora/{id}")]
-        public Editora Get(string id)
+        public EditoraDTO Get(string id)
         {
-            return _db.Editoras.Find(x => x.EditoraId == id).SingleOrDefault();
+            var editora = _db.Editoras.Find(x => x.EditoraId == id).SingleOrDefault();
+            return new EditoraDTO
+            {
+                Id = editora.EditoraId,
+                Nome = editora.Nome,
+                Email = editora.Email
+            };
         }
 
         // POST api/values
         [HttpPost("editora")]
-        public Editora Post([FromBody]Editora editora)
+        public EditoraDTO Post([FromBody]EditoraDTO dto)
         {
-            if (editora != null) 
+            if (dto != null)
+            {
+                var editora = new Editora
+                {
+                    Nome = dto.Nome,
+                    Email = dto.Email
+                };
                 _db.Editoras.InsertOne(editora);
-            return editora;        }
+                dto.Id = editora.EditoraId;
+            }
+            return dto;
+        }
 
         [HttpPut("editora/{id}")]
-        public Editora Put(string id, [FromBody]Editora editora)
+        public EditoraDTO Put(string id, [FromBody]EditoraDTO dto)
         {
-            if (editora != null)
+            if (dto != null)
+            {
+                var editora = _db.Editoras.Find(x => x.EditoraId == id).SingleOrDefault();
+                editora.Nome = dto.Nome;
+                editora.Email = dto.Email;
                 _db.Editoras.ReplaceOne(x => x.EditoraId == id, editora);
-            return editora;
+            }
+            return dto;
         }
 
     }
